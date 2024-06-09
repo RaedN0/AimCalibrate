5da733e3386c076b9d367e747c1a447cb5d52759
fhs48287 <mschoerghuber.its-b2021@fh-salzburg.ac.at>
2024-06-09 22:39:09 +0200

    Edit conf for building

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -1,3 +1,5 @@
+#![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
+
 use std::sync::{Arc, Mutex};
 use tauri::{GlobalShortcutManager, Manager, State, AppHandle};
 use enigo::{Enigo, MouseControllable};
