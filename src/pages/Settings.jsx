5bc442ec457d5fa6da797e1a11bd3be711255424
Marcel <marcel.schoerghuber@gmail.com>
2024-06-12 12:17:51 +0200

    Move turnspeed slider from sidebar to settings

--- src/pages/Settings.jsx
+++ src/pages/Settings.jsx
@@ -1,5 +1,6 @@
-import React, {useEffect, useState} from 'react';
+import React, {useEffect, useState, useCallback} from 'react';
 import {invoke} from '@tauri-apps/api/tauri';
+import debounce from 'lodash/debounce';
 
 function Settings() {
     const [hotkey, setHotkey] = useState('');
@@ -8,6 +9,28 @@ function Settings() {
     useEffect(() => {
         invoke('get_hotkey').then((hotkey) => setHotkey(hotkey));
     }, []);
+
+    const [sliderValue, setSliderValue] = useState(1);
+
+    useEffect(() => {
+        debouncedUpdateSettings(sliderValue)
+    }, [sliderValue]);
+
+    const handleSliderChange = (event) => {
+        setSliderValue(event.target.value);
+    };
+
+    const debouncedUpdateSettings = useCallback(
+        debounce((sliderValue) => {
+            console.log(sliderValue);
+            invoke('set_app_settings', {
+                turnSpeed: parseFloat(sliderValue)
+            }).catch((error) => {
+                console.error('Failed to set user settings:', error);
+            });
+        }, 500), // Debounce delay of 500ms
+        []
+    );
 
     const updateHotkey = (newHotkey) => {
         invoke('set_hotkey', {newHotkey}).then(() => setHotkey(newHotkey));
@@ -33,15 +56,30 @@ function Settings() {
     };
 
     return (
-        <div className="keybind-container" tabIndex="0"
-             onKeyDown={handleKeyPress}>
-            <div className="current-keybind">
-                Current Hotkey: {hotkey}
+        <div>
+            <div className="keybind-container" tabIndex="0"
+                 onKeyDown={handleKeyPress}>
+                <div className="current-keybind">
+                    Current Hotkey: {hotkey}
+                </div>
+                <button className="keybind-button" onClick={() => setSettingHotkey(true)}>
+                    {settingHotkey ? 'Press any key...' : 'Set Hotkey'}
+                </button>
             </div>
-            <button className="keybind-button" onClick={() => setSettingHotkey(true)}>
-                {settingHotkey ? 'Press any key...' : 'Set Hotkey'}
-            </button>
+            <div className="sidebar-slider">
+                <label htmlFor="sensitivity-slider">Turn speed: {sliderValue}</label>
+                <input
+                    type="range"
+                    id="sensitivity-slider"
+                    min="0.1"
+                    max="2"
+                    step="0.1"
+                    value={sliderValue}
+                    onChange={handleSliderChange}
+                />
+            </div>
         </div>
+
     );
 }
 
